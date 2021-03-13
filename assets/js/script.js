0e0683a061541a36be0dcb65c41fd7a64a5c7213
Drew Olsen <olsenj.andrew@gmail.com>
2021-03-13 11:25:51 -0600

    Initial html document setup. Additional planning.

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,9 +17,21 @@
 /*
 	1. Create global vars
 	2. Make an object of questions
+		- For starters, just a few questions for testing purposes
 	3. Make a timer
+		- less time for the beginning for testing purposes
 	4. Something to remove 10 seconds for a wrong answer
+		- This could be a simple if else comparing the correct answer and giving points if correct, removing if not.
 	5. Points system
+		- 10 seconds removed from timer for incorrect answer
+		- 5 points per correct answer
+		- 25 points for completing before timer expires
+		- Bonus points in the same amount as time remaining
 	6. Local storage save with clear
+	7. A function to update the score like that activity
+	8.Need a See High Scores button that will show the high scores panel.
+	9.Need a back to game button that will change it to the welcome game panel.
 
 */
+
+//Global Vars
