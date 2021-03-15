775fc356ace636d600b2012e736468de25c24c98
Drew Olsen <olsenj.andrew@gmail.com>
2021-03-15 10:34:57 -0500

    Refactered the views

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,10 +15,10 @@
 //Planning
 
 /*
-	1. Create global vars
-	2. Make an object of questions
+	1. DONE: Create global vars
+	2. DONE: Make an object of questions
 		- For starters, just a few questions for testing purposes
-	3. Make a timer
+	3. DONE: Make a timer
 		- less time for the beginning for testing purposes
 	4. Something to remove 10 seconds for a wrong answer
 		- This could be a simple if else comparing the correct answer and giving points if correct, removing if not.
@@ -52,7 +52,7 @@ var questions = {
 		c: "Mashed Potatoes",
 		d: "Cat"
 	},
-	q2: {
+	q3: {
 		question: "Who would you choose to marry?",
 		answer: "b",
 		a: "Betty White",
@@ -60,16 +60,15 @@ var questions = {
 		c: "Bill Burr",
 		d: "Cat"
 	}
-}
+};
 
 //Global Vars
-var score = "";
-var time = "";
+var score = 0;
+var timeLeft = 60;
 // Grabbing the elements I need
-var highScoresBtn = document.getElementById("show-scores");
+var showScoresBtn = document.querySelector("#showscores");
 var countdownTimer = document.getElementById("timer");
 var timeContainer = document.querySelector(".time-container")
-
 
 // this is the welcome view items
 var startBtn = document.getElementById("start");
@@ -78,6 +77,7 @@ var startBtn = document.getElementById("start");
 var questionText = document.getElementById("question-text");
 var possibleAnswersList = document.getElementById("possible-answers");
 var feedbackText = document.getElementById("feedback");
+var quitBtn = document.querySelector("button.quit");
 
 // This is the high scores page
 var highScoresList = document.querySelector(".high-scores");
@@ -89,10 +89,141 @@ var clearLeaderboard = document.getElementById("clear-leaderboard");
 
 
 // views
-var introView = document.querySelector(".intro");
-var questionsView = document.querySelector(".questions");
-var highScoresView = document.querySelector(".scoreboard");
-var correctAnswer = document.querySelector(".correct");
+var introWindow = document.querySelector(".intro");
+var questionsWindow = document.querySelector(".questions");
+var highScoresWindow = document.querySelector(".scoreboard");
+var correctAnswerBox = document.querySelector(".correct");
 
 
+//Globals
 
+function viewToggle(introView,questionsView,highScoreView) {
+	if (introView === "visible") {
+		introWindow.classList.remove("hidden");
+		introWindow.classList.add("visible");
+	} else {
+		introWindow.classList.remove("visible");
+		introWindow.classList.add("hidden");
+	}
+	
+	if (questionsView === "visible") {
+		questionsWindow.classList.remove("hidden");
+		questionsWindow.classList.add("visible");
+	} else {
+		questionsWindow.classList.remove("visible");
+		questionsWindow.classList.add("hidden");
+	};
+
+	if (highScoreView === "visible") {
+		highScoresWindow.classList.remove("hidden");
+		highScoresWindow.classList.add("visible");
+	} else {
+		highScoresWindow.classList.remove("visible");
+		highScoresWindow.classList.add("hidden");
+	}
+	
+};
+
+
+//Timer - this is going to be our countdown
+// Timer that counts down from 60. Yes I copied some of this from one of the activities but I do understand what it does. I also liked the comments for my future reference.
+function countdown() {
+	var timeLeft = 15;
+	// Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+	var timeInterval = setInterval(function () {
+		// As long as the `timeLeft` is greater than 1
+		if (timeLeft >= 1) {
+			// Set the `textContent` of `timerEl` to show the remaining seconds
+			timerEl.textContent = timeLeft;
+			// Decrement `timeLeft` by 1
+			timeLeft--;
+		} else {
+			// Once `timeLeft` gets to 0, set `timerEl` to an empty string
+			timerEl.textContent = '';
+
+			//Need to have something to calculate the score
+			
+			// Use `clearInterval()` to stop the timer
+			clearInterval(timeInterval);
+			// Call the `displayScoreboard()` function
+			introView();
+		}
+	}, 1000);
+};
+function changeHighScoreBtnText (label) {
+	showScoresBtn.textContent = label;
+}
+
+function initialSetup() {
+	score = 0;
+	timeLeft = 60;
+	
+	if (highScoresWindow.classList.contains("visible") || questionsWindow.classList.contains("visible")) {
+		//Hides the scores view, hides the questions view, shows intro
+		introView();
+	}
+	changeHighScoreBtnText("High Scores");
+
+}
+
+function gameOver() {
+
+};
+
+//function to remove 10 when an incorrect answer occurs
+function wrongAnswer() {	
+	if (timeLeft < 11) {
+		gameOver();
+	} else {
+		timeleft = timeLeft - 10;
+	}
+};
+// ALLL THE VIEWS
+function introView() {
+	console.log("Intro View Showing.");
+	viewToggle("visible", "hidden", "hidden");
+	changeHighScoreBtnText("High Scores");
+};
+//Also call this when the game ends
+function highScoresView() {
+	console.log("High Scores View Showing.");
+	//Shows the scores view, hides the questions view, hides intro
+	viewToggle("hidden", "hidden", "visible");
+	changeHighScoreBtnText("Back");
+};
+
+function questionsView() {
+	console.log("Questions View Showing.");
+	changeHighScoreBtnText("High Scores");
+	
+	//Hides the scores view, hides the questions view, shows intro
+	viewToggle("hidden","visible","hidden");
+
+};
+
+showScoresBtn.addEventListener( "click", function(e) {
+	console.log(e);
+	if (showScoresBtn.textContent === "High Scores") {
+		highScoresView();
+	} else {
+		introView();
+	}
+} );
+
+startBtn.addEventListener("click", function(e) {
+	console.log(e);
+	questionsView();
+});
+
+quitBtn.addEventListener("click", function(e) {
+	console.log(e);
+	initialSetup();
+});
+
+playAgain.addEventListener("click", function (e) {
+	console.log(e);
+	initialSetup();
+});
+
+
+ 
